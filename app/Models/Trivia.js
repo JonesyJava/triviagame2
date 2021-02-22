f1f6f5eb2a5a14275bb6a3683460cbe940f81ede
jonesyjava <jonesyjava@gmail.com>
2021-02-22 15:45:38 -0700

    updated trivia challenge bootstrap

--- app/Models/Trivia.js
+++ app/Models/Trivia.js
@@ -15,15 +15,15 @@ export default class Trivia {
             <hr>
             <h5>Question:</h5> ${this.question}
             <hr>
-            <h5 class="hidden" id="revealAnswer">Answer: ${this.correct_answer}</h5>
+            <h5 class="text-success">Answer: ${this.correct_answer}</h5>
             <hr>
-            <h5 class="hidden">Incorrect Answer: ${this.incorrect_answers}</h5>
+            <h5 class="text-danger">Incorrect Answer: ${this.incorrect_answers}</h5>
             <hr>
             <h5>Difficulty:</h5> ${this.difficulty}
             <hr>
-            
-            <button class="btn btn-primary mx-5 my-2 text-center" onclick="app.triviaController.previous()">Previous</button>
-            <button class="btn btn-primary mx-5 my-2 text-center" onclick="app.triviaController.next()">Next</button>
+
+            <button class="btn btn-dark mx-5 my-2 text-center" onclick="app.triviaController.previous()">Previous</button>
+            <button class="btn btn-dark mx-5 my-2 text-center" onclick="app.triviaController.next()">Next</button>
         </col-3>
         `
     }
